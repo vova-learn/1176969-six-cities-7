e9c4bea211ecdb5c519b9cee8cca77fbe3f9d125
vovapipko <vpmail13@gmail.com>
2021-06-11 22:24:50 +0300

    Опишет тесты для маршрутизации

--- project/src/components/app/app.spec.js
+++ project/src/components/app/app.spec.js
@@ -1,9 +1,87 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './app';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { AppRoute } from '../../consts';
 
 describe('App', () => {
   it('renders App component', () => {
-    render(<App />);
+    const history = createMemoryHistory();
+    const placesCount = 312;
+    history.push(AppRoute.SIGN_IN);
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
   });
+});
+
+describe('Test routing', () => {
+  it('Render MainScreen when user navigate to "/" url', () => {
+    const history = createMemoryHistory();
+    const placesCount = 312;
+
+    history.push(AppRoute.MAIN);
+
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
+
+    expect(screen.getByAltText(/6 cities logo/i)).toBeInTheDocument();
+  });
+
+  it('Render SignInScreen when user navigate to "/login" url', () => {
+    const history = createMemoryHistory();
+    const placesCount = 312;
+
+    history.push(AppRoute.SIGN_IN);
+
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
+
+    expect(screen.getAllByText(/sign in/i)[0]).toBeInTheDocument();
+  });
+
+  it('Render FavoritesScreen when user navigate to "/favorites" url', () => {
+    const history = createMemoryHistory();
+    const placesCount = 312;
+
+    history.push(AppRoute.FAVORITES);
+
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
+  });
+
+  it('Render RoomScreen when user navigate to "/offer/:id" url', () => {
+    const history = createMemoryHistory();
+    const placesCount = 312;
+
+    history.push(AppRoute.ROOM);
+
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
+  });
+
+  it('Render NotFoundScreen when user navigate to "/non-existent" route', () => {
+    const history = createMemoryHistory();
+    const placesCount = 312;
+
+    history.push(AppRoute.NOT_FOUND);
+
+    render(
+      <Router history={history} >
+        <App placesCount={placesCount}/>
+      </Router>);
+  });
+
+  expect(screen.getByText(/404/i)).toBeInTheDocument();
+  expect(screen.getByText(/Go back to the main page/i)).toBeInTheDocument();
 });
--- project/src/consts.js
+++ project/src/consts.js
@@ -1,3 +1,11 @@
 export const AppElement = {
   ROOT: document.getElementById('root'),
 };
+
+export const AppRoute = {
+  MAIN: '/',
+  SIGN_IN: '/login',
+  FAVORITES: '/favorites',
+  ROOM: '/offer/',
+  NOT_FOUND: '/non-existent-route',
+};
