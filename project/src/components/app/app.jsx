1043ec87200d869fe7195924dd8d17e23371cabd
vovapipko <vpmail13@gmail.com>
2021-06-11 22:27:34 +0300

    Импортирует BrowserRouter Switch Route WIP

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MainScreen from '../main-screen/main-screen';
-
+import { Route, Switch } from 'react-router';
 function App({placesCount}) {
   return <MainScreen placesCount={placesCount} />;
 }
